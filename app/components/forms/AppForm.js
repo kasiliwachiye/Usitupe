a8d0b564f551ae2d07fadb6384f6cfe554a91818
Emmanuel Wachiye <wachiye25@gmail.com>
2021-07-06 00:42:20 +0300

    sorting components (form)

---
+++ app/components/forms/AppForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Formik } from 'formik'
+
+export default function AppForm({ initialValues, onSubmit, validationSchema, children }) {
+    return (
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+            {() => <React.Fragment>{children}</React.Fragment> }
+        </Formik>
+    )
+}
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { StyleSheet, Image } from 'react-native'
-import { Formik } from 'formik'
 import * as Yup from 'yup'
 
 import Screen from '../components/Screen'
-import AppFormField from '../components/AppFormField'
-import SubmitButton from '../components/SubmitButton'
+import { AppForm, AppFormField, SubmitButton} from '../components/forms'
+
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -16,15 +15,11 @@ export default function LoginScreen() {
     return (
         <Screen style={styles.container}>
             <Image style={styles.logo} source={require('../assets/logo.png')}/>
-            <Formik initialValues={{email: '', password: ''}} onSubmit={values => console.log(values)} validationSchema={validationSchema}>
-                {() => (
-                    <React.Fragment>
-                        <AppFormField autoCapitalize="none" autoCorrect={false}  icon="mail" keyboardType="email-address" name='email' placeholder="Email" textContentType="emailAddress" />
-                        <AppFormField autoCapitalize="none" autoCorrect={false} icon="lock" name="password" placeholder="Password" secureTextEntry textContentType="password" />
-                        <SubmitButton title="Login" />
-                    </React.Fragment>
-                    )}
-            </Formik>
+            <AppForm initialValues={{email: '', password: ''}} onSubmit={values => console.log(values)} validationSchema={validationSchema}>
+                <AppFormField autoCapitalize="none" autoCorrect={false}  icon="mail" keyboardType="email-address" name='email' placeholder="Email" textContentType="emailAddress" />
+                <AppFormField autoCapitalize="none" autoCorrect={false} icon="lock" name="password" placeholder="Password" secureTextEntry textContentType="password" />
+                <SubmitButton title="Login" />
+            </AppForm>
         </Screen>
     )
 }
